dd7baf7630af00e5c8bd0dec10dd7a4b133bd2d6
Luis Felipe Maciel <luis.barbosa@al.infnet.edu.br>
2022-11-23 18:00:40 -0300

    Implentação dos teste em ProfessorTeste

---
+++ App/src/br/infnet/edu/testes/ProfessorTeste.java
@@ -0,0 +1,27 @@
+package br.infnet.edu.testes;
+
+import br.infnet.edu.dominio.Professor;
+
+public class ProfessorTeste {
+
+	public static void main(String[] args) {
+		
+		Professor prof1 = new Professor();
+		prof1.setNome("Alisson");
+		prof1.setSobrenome("Blois");
+		prof1.setEmail("alisson.blois@prof.infnet.edu.br");
+		prof1.setDisciplina("Java");
+
+		Professor prof2 = new Professor("Bruno", "Silva");
+		prof2.setEmail("bruno.silva@prof.infnet.edu.br");
+		prof2.setDisciplina("Front-End");
+
+		Professor prof3 = new Professor("Gabriel", "Vicente", "gabriel@prof.infnet.edu.br", "Android");
+			
+		prof1.consultarSituacao(1);
+		prof2.consultarSituacao(2);
+		prof3.consultarSituacao(3);
+
+	}
+
+}
